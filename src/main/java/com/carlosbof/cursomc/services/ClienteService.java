85e4ed01aec7a8c6c4a927efa673d309bc5764a8
Carlos Eduardo Bof <5-cbof@users.noreply.gitlab.token.local>
2021-12-02 13:51:23 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/carlosbof/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.carlosbof.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosbof.cursomc.domain.Cliente;
+import com.carlosbof.cursomc.exceptions.ObjectNotFoundException;
+import com.carlosbof.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: (" + id + "), Tipo: " + Cliente.class.getName()));
+	}
+	
+}
