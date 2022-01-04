020df72df7557a3af733798841fb949a8884b626
Carlos Eduardo Bof <5-cbof@users.noreply.gitlab.token.local>
2022-01-04 18:24:37 -0300

    Validacao Sintatica com o Bean Validation

--- src/main/java/com/carlosbof/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/carlosbof/cursomc/resources/CategoriaResource.java
@@ -15,6 +15,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import com.carlosbof.cursomc.domain.Categoria;
 import com.carlosbof.cursomc.dto.CategoriaDTO;
 import com.carlosbof.cursomc.services.CategoriaService;
@@ -32,16 +34,20 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 		          .path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
+
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
